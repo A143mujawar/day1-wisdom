80a98c7a220ad68c9041fe2e69244052befa4d1d
A143mujawar <156198182+A143mujawar@users.noreply.github.com>
2025-08-15 20:44:34 +0530

    Create day1.py
    day1 challenge

---
+++ day1.py
@@ -0,0 +1,16 @@
+def day1(arr): 
+    low, mid, high = 0, 0, len(arr) - 1
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 2:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+        else:
+            mid += 1
+
+nums = [2, 0, 2, 1, 1, 0]
+day1(nums) 
+print(nums)
