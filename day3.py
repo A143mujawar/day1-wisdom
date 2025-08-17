82a66489cf16673876589d2e0a84fc192e649c6e
A143mujawar <156198182+A143mujawar@users.noreply.github.com>
2025-08-17 20:01:02 +0530

    Create day3.py
    day3 challenge

---
+++ day3.py
@@ -0,0 +1,23 @@
+def day3(nums):
+    
+    slow = nums[0]
+    fast = nums[0]
+
+    while True:
+        slow = nums[slow]        
+        fast = nums[nums[fast]]   
+        if slow == fast:
+            break
+
+   
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+
+    return slow
+
+
+
+arr = [3, 1, 3, 4, 2]
+print("Duplicate number:", day3(arr))
